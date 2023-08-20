75b75f38e4dbf06cd79d918357a8f4be43161827
Jhean Bryan <93744970+jheanbryan@users.noreply.github.com>
2023-08-19 21:58:16 -0400

    update modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -5,12 +5,18 @@ function openModal(projeto){
         imgModal.src = 'assets/thumb/handson.png';
         imgModal.alt = "site HandsOn";
 
+        tituloDoProjeto.innerHTML = 'HandsOn'
+        DescricaoDoProjeto.innerHTML = 'bla bla'
+
         modal.style.display = 'flex'
         modal.show();
     }
     if (projeto === 'cadastro-funcionarios'){
         imgModal.src = 'assets/thumb/cadastro-funcionarios.png';
         imgModal.alt = "site cadastro de funcionarios";
+
+        tituloDoProjeto.innerHTML = 'Cadastro de funcionarios';
+        DescricaoDoProjeto.innerHTML = 'bla bla'
 
         modal.style.display = 'flex'
         modal.show();
@@ -19,12 +25,18 @@ function openModal(projeto){
         imgModal.src = 'assets/thumb/calculadora-imc.png';
         imgModal.alt = "site de calculadora de imc";
 
+        tituloDoProjeto.innerHTML = 'Calculdora de IMC'
+        DescricaoDoProjeto.innerHTML = 'bla bla'
+
         modal.style.display = 'flex'
         modal.show();
     }
     if (projeto === 'gerador-de-senha'){
         imgModal.src = 'assets/thumb/gerador-de-senha.png';
         imgModal.alt = "site gerador de senha";
+
+        tituloDoProjeto.innerHTML = 'Gerador de Senha'
+        DescricaoDoProjeto.innerHTML = 'bla bla'
 
         modal.style.display = 'flex'
         modal.show();
