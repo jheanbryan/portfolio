76996d73aae85434ba3f2256e136e46c6017c04d
JHE4N <93744970+JHE4N@users.noreply.github.com>
2023-12-19 11:55:40 -0400

    update in js code
    removed manipulation with Dom and added manipulation with css class

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,63 +2,74 @@ const navbarItens = document.getElementById('itens'); // div do navbar
 const xCloseMenu = document.getElementById('xMenu'); //X do menu
 const barsOpenMen = document.getElementById('burguerMenu'); //Barras de abrir menu
 const itenMenu = document.querySelectorAll('.navBarIten'); //navbar menu
-
 const dark = document.getElementById('darkMode'); //modo dark
 const light = document.getElementById('lightMode'); //modo claro
-
 const arrowUp = document.getElementById('containerArrowUp');
 
 function openMenu(){
     navbarItens.classList.add('visible');
     xCloseMenu.classList.add('block');
     barsOpenMen.classList.add('none');
-
 }
 
 function closeMenu(){
     navbarItens.classList.remove('visible');
     xCloseMenu.classList.remove('block');
     barsOpenMen.classList.remove('none');
-
 }
 
 function alterTheme(theme){
     if (theme === 'dark') {
-        darkMode.style.display = 'none';
-        lightMode.style.display = 'block';
+        light.classList.remove('none');
+        dark.classList.remove('block');
         document.querySelector('html').classList.toggle('light-mode');
 
     } else if (theme === 'light') {
-        darkMode.style.display = 'block';
-        lightMode.style.display = 'none';
+        light.classList.add('none');
+        dark.classList.add('block');
         document.querySelector('html').classList.toggle('light-mode');
-    }
-
-
-}
+    };
+};
 
 //menu
-barsOpenMen.addEventListener('click', openMenu);
-xCloseMenu.addEventListener('click', closeMenu);
-
-//dark mode
-dark.addEventListener('click', () => alterTheme('dark'));
-light.addEventListener('click', () => alterTheme('light'));
-
-//fechar menu caso tenha click
-for (let i = 0; i < itenMenu.length; i++) {
-    itenMenu[i].addEventListener('click', closeMenu);
+function addEventInMenu() {
+    barsOpenMen.addEventListener('click', openMenu);
+    xCloseMenu.addEventListener('click', closeMenu);
 }
 
+//dark mode
+function addEventInIconTheme(params) {
+    dark.addEventListener('click', () => alterTheme('dark'));
+    light.addEventListener('click', () => alterTheme('light'));
+}
+
+//fechar menu caso tenha click
+function AddEventForClickInMenuItem(params) {
+    for (let i = 0; i < itenMenu.length; i++) {
+        itenMenu[i].addEventListener('click', closeMenu);
+    };
+};
+
 //Arrow up
-window.addEventListener('scroll', function() {
-    var scrollVertical = window.scrollY || document.documentElement.scrollTop;
+function visibilityInArrowUp() {
+    window.addEventListener('scroll', function() {
+        var scrollVertical = window.scrollY || document.documentElement.scrollTop;
+    
+        if (scrollVertical >= 1250) {
+            arrowUp.classList.add('block');
+    
+        } else {
+            arrowUp.classList.remove('block');
+    
+        };
+    });
+}
 
-    if (scrollVertical >= 1250) {
-        arrowUp.classList.add('block');
+function init() {
+    addEventInMenu();
+    AddEventForClickInMenuItem();
+    addEventInIconTheme();
+    visibilityInArrowUp();
+}
 
-    } else {
-        arrowUp.classList.remove('block');
-
-    }
-});
+init();
