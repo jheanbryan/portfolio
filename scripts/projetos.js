66f3bf7dd1a779191c8c331c2ee401945abe1d41
Jhean Bryan <93744970+jheanbryan@users.noreply.github.com>
2023-12-26 17:27:58 -0400

    Update projetos.js

--- scripts/projetos.js
+++ scripts/projetos.js
@@ -44,7 +44,7 @@ export const projetosWeb = [
         name: 'magazine-hashtag',
         title: 'Ecomerce',
         path: 'assets/thumb/magazine-hashtag.png',
-        link: 'https://magazine-hashtag-livid.vercel.app',
+        link: 'https://magazine-hashtag-html-css-js.vercel.app',
         repo: 'https://github.com/jheanbryan/magazine-hashtag',
         description:'Ecomerce construído durante o intensivão de JavaScript da Hashtag. Esse projeto utiliza o localStorage como banco de dados para salvar as informações do usuário, tais como histórico de compras e itens adicionados ao carrinho.',
         alt: 'Ecomerce',
